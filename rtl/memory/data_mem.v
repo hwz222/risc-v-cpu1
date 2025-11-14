7ee2509202d7dbac2ca59c2d5e5daec6afb63d0e
hwj <jackhuang.miat@gmail.com>
2025-11-14 17:26:47 +0800

    Implement instrucion memroy and data memory

---
+++ rtl/memory/data_mem.v
@@ -0,0 +1,28 @@
+module data_mem #(
+    parameter DEPTH = 256
+)(
+    input  wire        clk,
+    input  wire        we,
+    input  wire [3:0]  strobe,
+    input  wire [31:0] addr,   // byte address
+    input  wire [31:0] wdata,
+    output wire [31:0] rdata
+);
+    reg [31:0] mem [0:DEPTH-1];
+
+    wire [31:0] word_addr = addr[31:2];
+
+    // synchronous write
+    always @(posedge clk) begin
+        if (we) begin
+            if(strobe[0]) mem[word_addr][7:0] <= wdata[7:0];
+            if(strobe[1]) mem[word_addr][15:8] <= wdata[15:8];
+            if(strobe[2]) mem[word_addr][23:16] <= wdata[23:16];
+            if(strobe[3]) mem[word_addr][31:24] <= wdata[31:24];
+        end
+    end
+
+    // async read
+    assign rdata = mem[word_addr];
+
+endmodule
---
+++ rtl/memory/instr_mem.v
@@ -0,0 +1,18 @@
+module instr_mem #(
+    parameter DEPTH = 256
+)(
+    input  wire [31:0] addr,   // byte address
+    output wire [31:0] instr
+);
+    // 用 reg 陣列描述 ROM
+    reg [31:0] mem [0:DEPTH-1];
+
+    // async read：addr 一變，instr 會經過 LUT delay 後跟著變
+    assign instr = mem[addr[31:2]];  // word address = addr[31:2]
+
+    // 模擬／綁定程式用
+    initial begin
+        $readmemh("prog.hex", mem);
+    end
+
+endmodule
